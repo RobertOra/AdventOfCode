4822af411404f34a5ecb83ca04bdc1b719932a43
Robert Oratorio <robert@LAPTOP-EL02NGPR.localdomain>
2025-12-01 18:12:43 -0500

    day 1 part 2

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,14 +1,28 @@
 curr = 50
 res = 0
 for line in open("day1_input.txt", 'r'):
+    prev = curr
     direction, distance = line[0], int(line[1:])
+
+    res += distance // 100
+    distance %= 100
+    if distance == 0:
+        continue
+
     if direction == 'L':
         curr -= distance
     else:
         curr += distance
-
     curr %= 100
+
+    if prev == 0:
+        continue
+
     if curr == 0:
+        res += 1
+    elif direction == 'L' and curr > prev:
+        res += 1
+    elif direction == 'R' and curr < prev:
         res += 1
 print(res)
 
