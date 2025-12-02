0df440d165361d1acf61913c68b10d2ac3e4fd3e
Robert Oratorio <robert@LAPTOP-EL02NGPR.localdomain>
2025-12-02 16:03:57 -0500

    day 2 part 2 optimized

--- 2025/day2.py
+++ 2025/day2.py
@@ -8,34 +8,12 @@ for id_range in line.split(','):
     # sum all invalid ids between first_id and last_id (inclusive)
     # invalid id - digits repeated twice
 
-    #brute force
+    # optimized
     for id in range(first_id, last_id + 1):
         id_str = str(id)
-        for sub_len in range(len(id_str) // 2, 0, -1):
-            if len(id_str) % sub_len == 0:
-                sub = id_str[:sub_len]
-                ind = 0
-
-                invalid = True
-                while ind < len(id_str):
-                    if id_str[ind:ind + sub_len] != sub:
-                        invalid = False
-                        break
-                    ind += sub_len
-
-                if invalid:
-                    invalid_id_sum += id
-                    break
-
-        '''
-        part 1
-        if len(id_str) % 2 == 1:
-            continue
-        mid = len(id_str) // 2
-        left_half = id_str[:mid]
-        right_half = id_str[mid:]
-        if left_half == right_half:
+        repeat_check_str = id_str*2
+        repeat_check_str = repeat_check_str[1:-1]
+        if id_str in repeat_check_str:
             invalid_id_sum += id
-        '''
 print(invalid_id_sum)
 
