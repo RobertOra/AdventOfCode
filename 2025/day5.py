4f409fa1842bd987502f0ced637977e008de86e4
Robert Oratorio <robertoratorio@gmail.com>
2025-12-05 19:17:31 -0500

    day 5 part 2

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,42 +1,25 @@
 ranges = []  # list of fresh ingredients ranges, inclusive
-ids = []  # list of ingredient ids
 with open("day5_input.txt", 'r') as f:
-    lines = f.read().splitlines()
-
-for line in lines:
-    split_line = line.strip().split('-')
-    if len(split_line) == 2:
-        ranges.append(list(map(int, split_line)))
-    else:
-        if split_line[0] == "":
-            continue
-        ids.append(int(split_line[0]))
-
-#print(ranges)
-#print(ids)
-#print number of fresh ingredient ids
-# first, combine the ranges
-ranges.sort()
-ranges_combined = []
-for interval in ranges:
-    if not ranges_combined:
-        ranges_combined.append(interval)
-        continue
-    if ranges_combined[-1][1] >= interval[0]:
-        ranges_combined[-1][1] = max(ranges_combined[-1][1], interval[1])
-    else:
-        ranges_combined.append(interval)
-ranges = ranges_combined
-
-# count how many ids fall into one of the ranges
-
-# more efficiently you could binary search or
-# sort ids beforehand so you wouldn't need to iterate
-# through all ranges every time
-fresh = 0
-for id in ids:
-    for l, r in ranges:
-        if l <= id <= r:
-            fresh += 1
+    for line in f:
+        split_line = line.strip().split('-')
+        if len(split_line) == 2:
+            ranges.append(list(map(int, split_line)))
+        else:
             break
-print(fresh)
+# sum the number of fresh ingredient ids that fall in the ranges
+
+#combine the ranges
+ranges.sort()
+curr = ranges[0]
+res = ranges[0][1] - ranges[0][0] + 1
+for i in range(1, len(ranges)):
+    interval = ranges[i]
+    if curr[1] >= interval[0]:
+        if interval[1] > curr[1]:
+            res += interval[1] - curr[1]
+            curr[1] = interval[1]
+    else:
+        curr = interval
+        res += curr[1] - curr[0] + 1
+
+print(res)
